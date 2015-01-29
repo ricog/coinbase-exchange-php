89b59e546b1f02a6e677304645e0499708d1654f
Rick Guyer <ricoguyer@gmail.com>
2015-01-28 21:09:17 -0600

    Tab to space changes

--- lib/CoinbaseExchange/CoinbaseExchange.php
+++ lib/CoinbaseExchange/CoinbaseExchange.php
@@ -7,15 +7,15 @@
  */
 class CoinbaseExchange {
 
-	/**
-	 * API Endpoint URL
-	 */
-	public $url = 'https://api.exchange.coinbase.com';
+    /**
+     * API Endpoint URL
+     */
+    public $url = 'https://api.exchange.coinbase.com';
 
-	/**
-	 * An array of API endpoints
-	 */
-	public $endpoints = array(
+    /**
+     * An array of API endpoints
+     */
+    public $endpoints = array(
         'accounts' => array('method' => 'GET', 'uri' => '/accounts'),
         'account' => array('method' => 'GET', 'uri' => '/accounts/%s'),
         'ledger' => array('method' => 'GET', 'uri' => '/accounts/%s/ledger'),
@@ -31,9 +31,9 @@ class CoinbaseExchange {
         'trades' => array('method' => 'GET', 'uri' => '/products/%s/trades'),
         'stats' => array('method' => 'GET', 'uri' => '/products/%s/stats'),
         'rates' => array('method' => 'GET', 'uri' => '/products/%s/candles'),
-	    'currencies' => array('method' =>'GET', 'uri' =>  '/currencies'),
-		'time' => array('method' => 'GET', 'uri' => '/time'),
-	);
+        'currencies' => array('method' =>'GET', 'uri' =>  '/currencies'),
+        'time' => array('method' => 'GET', 'uri' => '/time'),
+    );
 
     /**
      * Headers to send with each call
@@ -62,47 +62,47 @@ class CoinbaseExchange {
     }
 
     /**
-	 * GET /accounts
-	 *
-	 * https://docs.exchange.coinbase.com/#list-accounts
-	 */
-	public function listAccounts() {
-		return $this->request('accounts');
-	}
+     * GET /accounts
+     *
+     * https://docs.exchange.coinbase.com/#list-accounts
+     */
+    public function listAccounts() {
+        return $this->request('accounts');
+    }
 
-	/**
-	 * GET /accounts/<account-id>
-	 *
-	 * https://docs.exchange.coinbase.com/#get-an-account
-	 */
-	public function getAccount($id) {
-		return $this->request('account', array('id' => $id));
-	}
+    /**
+     * GET /accounts/<account-id>
+     *
+     * https://docs.exchange.coinbase.com/#get-an-account
+     */
+    public function getAccount($id) {
+        return $this->request('account', array('id' => $id));
+    }
 
-	/**
-	 * GET /accounts/<account-id>/ledger
-	 *
-	 * https://docs.exchange.coinbase.com/#get-account-history
-	 */
-	public function getAccountHistory($id) {
-		return $this->request('ledger', array('id' => $id));
-	}
+    /**
+     * GET /accounts/<account-id>/ledger
+     *
+     * https://docs.exchange.coinbase.com/#get-account-history
+     */
+    public function getAccountHistory($id) {
+        return $this->request('ledger', array('id' => $id));
+    }
 
-	/**
-	 * GET /accounts/<account_id>/holds
-	 *
-	 * https://docs.exchange.coinbase.com/#get-holds
-	 */
-	public function getHolds($id) {
-		return $this->request('holds', array('id' => $id));
-	}
+    /**
+     * GET /accounts/<account_id>/holds
+     *
+     * https://docs.exchange.coinbase.com/#get-holds
+     */
+    public function getHolds($id) {
+        return $this->request('holds', array('id' => $id));
+    }
 
-	/**
-	 * POST /orders
-	 *
-	 * https://docs.exchange.coinbase.com/#place-a-new-order
-	 */
-	public function placeOrder($side, $price, $size, $productId) {
+    /**
+     * POST /orders
+     *
+     * https://docs.exchange.coinbase.com/#place-a-new-order
+     */
+    public function placeOrder($side, $price, $size, $productId) {
         $data = array(
             //'client_oid' => '', // client generated UUID
             'price' => $price, // in quote_increment units (0.01 min for BTC-USD)
@@ -111,132 +111,132 @@ class CoinbaseExchange {
             'product_id' => $productId
             //'stp' => 'dc' // Or one of co, cn, cb
         );
-		return $this->request('place', $data);
-	}
+        return $this->request('place', $data);
+    }
 
-	/**
-	 * DELETE /orders/<order-id>
-	 *
-	 * https://docs.exchange.coinbase.com/#cancel-an-order
-	 */
-	public function cancelOrder($id) {
-		return $this->request('cancel', array('id' => $id));
-	}
+    /**
+     * DELETE /orders/<order-id>
+     *
+     * https://docs.exchange.coinbase.com/#cancel-an-order
+     */
+    public function cancelOrder($id) {
+        return $this->request('cancel', array('id' => $id));
+    }
 
-	/**
-	 * GET /orders
-	 *
-	 * https://docs.exchange.coinbase.com/#list-open-orders
-	 */
-	public function listOrders() {
-		return $this->request('orders');
-	}
-
-
-	/**
-	 * GET /orders/<order-id>
-	 *
-	 * https://docs.exchange.coinbase.com/#get-an-order
-	 */
-	public function getOrder($id) {
-		return $this->request('order', array('id' => $id));
-	}
-
-	/**
-	 * GET /fills
-	 *
-	 * https://docs.exchange.coinbase.com/#fills
-	 */
-	public function listFills() {
-		return $this->request('fills');
-	}
+    /**
+     * GET /orders
+     *
+     * https://docs.exchange.coinbase.com/#list-open-orders
+     */
+    public function listOrders() {
+        return $this->request('orders');
+    }
 
 
-	/**
-	 * POST /transfers
-	 *
-	 * https://docs.exchange.coinbase.com/#transfer-funds
-	 */
-	public function transferFunds() {
-	}
+    /**
+     * GET /orders/<order-id>
+     *
+     * https://docs.exchange.coinbase.com/#get-an-order
+     */
+    public function getOrder($id) {
+        return $this->request('order', array('id' => $id));
+    }
 
-	/**
-	 * GET /products
-	 *
-	 * https://docs.exchange.coinbase.com/#products
-	 */
-	public function products() {
-		return $this->request('products');
-	}
+    /**
+     * GET /fills
+     *
+     * https://docs.exchange.coinbase.com/#fills
+     */
+    public function listFills() {
+        return $this->request('fills');
+    }
 
 
-	/**
-	 * GET /products/<product-id>/book
-	 *
-	 * https://docs.exchange.coinbase.com/#get-product-order-book
-	 */
-	public function getOrderBook($product = 'BTC-USD') {
+    /**
+     * POST /transfers
+     *
+     * https://docs.exchange.coinbase.com/#transfer-funds
+     */
+    public function transferFunds() {
+    }
+
+    /**
+     * GET /products
+     *
+     * https://docs.exchange.coinbase.com/#products
+     */
+    public function products() {
+        return $this->request('products');
+    }
+
+
+    /**
+     * GET /products/<product-id>/book
+     *
+     * https://docs.exchange.coinbase.com/#get-product-order-book
+     */
+    public function getOrderBook($product = 'BTC-USD') {
         //$this->validate('product', $product);
-		return $this->request('book', array('id' => $product));
-	}
+        return $this->request('book', array('id' => $product));
+    }
 
 
-	/**
-	 * GET /products/<product-id>/ticker
-	 *
-	 * https://docs.exchange.coinbase.com/#get-product-ticker
-	 */
-	public function getTicker($product = 'BTC-USD') {
-		return $this->request('ticker', array('id' => $product));
-	}
+    /**
+     * GET /products/<product-id>/ticker
+     *
+     * https://docs.exchange.coinbase.com/#get-product-ticker
+     */
+    public function getTicker($product = 'BTC-USD') {
+        return $this->request('ticker', array('id' => $product));
+    }
 
-	/**
-	 * GET /products/<product-id>/trades
-	 *
-	 * https://docs.exchange.coinbase.com/#get-trades
-	 */
-	public function listTrades($product = 'BTC-USD') {
-		return $this->request('trades', array('id' => $product));
-	}
+    /**
+     * GET /products/<product-id>/trades
+     *
+     * https://docs.exchange.coinbase.com/#get-trades
+     */
+    public function listTrades($product = 'BTC-USD') {
+        return $this->request('trades', array('id' => $product));
+    }
 
-	/**
-	 * GET /products/<product-id>/candles
-	 *
-	 * https://docs.exchange.coinbase.com/#get-historic-rates
-	 */
-	public function getHistoricRates($product = 'BTC-USD') {
-		return $this->request('rates', array('id' => $product));
-	}
+    /**
+     * GET /products/<product-id>/candles
+     *
+     * https://docs.exchange.coinbase.com/#get-historic-rates
+     */
+    public function getHistoricRates($product = 'BTC-USD') {
+        return $this->request('rates', array('id' => $product));
+    }
 
-	/**
-	 * GET /products/<product-id>/stats
-	 *
-	 * https://docs.exchange.coinbase.com/#get-24hr-stats
-	 */
-	public function get24hrStats($product = 'BTC-USD') {
-		return $this->request('stats', array('id' => $product));
-	}
+    /**
+     * GET /products/<product-id>/stats
+     *
+     * https://docs.exchange.coinbase.com/#get-24hr-stats
+     */
+    public function get24hrStats($product = 'BTC-USD') {
+        return $this->request('stats', array('id' => $product));
+    }
 
-	/**
-	 * GET /currencies
-	 *
-	 * https://docs.exchange.coinbase.com/#get-currencies
-	 */
-	public function listCurrencies() {
-		return $this->request('currencies');
-	}
+    /**
+     * GET /currencies
+     *
+     * https://docs.exchange.coinbase.com/#get-currencies
+     */
+    public function listCurrencies() {
+        return $this->request('currencies');
+    }
 
-	/**
-	 * GET /time
-	 *
-	 * https://docs.exchange.coinbase.com/#time
-	 */
-	public function getTime() {
-		return $this->request('time');
-	}
+    /**
+     * GET /time
+     *
+     * https://docs.exchange.coinbase.com/#time
+     */
+    public function getTime() {
+        return $this->request('time');
+    }
 
-	protected function request($endpoint, $params = array()) {
-		extract($this->getEndpoint($endpoint, $params));
+    protected function request($endpoint, $params = array()) {
+        extract($this->getEndpoint($endpoint, $params));
         $url = $this->url . $uri;
         $body = (!empty($params) ? json_encode($params) : '');
         $headers = array(
@@ -258,9 +258,9 @@ class CoinbaseExchange {
         } catch (Exception $e) {
             return 'Caught exception: ' . $e->getMessage();
         }
-	}
+    }
 
-	protected function getEndpoint($key, $params) {
+    protected function getEndpoint($key, $params) {
         $endpoint = $this->endpoints[$key];
         if (empty($endpoint)) {
             throw new Exception('Invalid endpoint ' . $key . ' specified');
@@ -271,7 +271,7 @@ class CoinbaseExchange {
         }
         $endpoint['params'] = $params;
         return $endpoint;
- 	}
+    }
 
     protected function sign($data) {
         $this->timestamp = time();
--- lib/CoinbaseExchange/Request.php
+++ lib/CoinbaseExchange/Request.php
@@ -1,13 +1,13 @@
 <?php
 
 class CoinbaseExchange_Request {
-	public function call($url, $method, $headers, $body = '') {
-		$curl = curl_init();
-		$options = array(
-			CURLOPT_URL => $url,
-			CURLOPT_HTTPHEADER => $headers,
-			CURLOPT_RETURNTRANSFER => true,
-		);
+    public function call($url, $method, $headers, $body = '') {
+        $curl = curl_init();
+        $options = array(
+            CURLOPT_URL => $url,
+            CURLOPT_HTTPHEADER => $headers,
+            CURLOPT_RETURNTRANSFER => true,
+        );
 
         $method = strtolower($method);
         if ($method == 'get') {
@@ -22,20 +22,20 @@ class CoinbaseExchange_Request {
             $options[CURLOPT_CUSTOMREQUEST] = "PUT";
             $options[CURLOPT_POSTFIELDS] = $body;
         }
-		curl_setopt_array($curl, $options);
-		$response = curl_exec($curl);
-		if ($response === false) {
-			$error = curl_errno($curl);
-			$message = curl_error($curl);
-			curl_close($curl);
-			throw new Exception("Network error " . $message . " (" . $error . ")");
-		}
+        curl_setopt_array($curl, $options);
+        $response = curl_exec($curl);
+        if ($response === false) {
+            $error = curl_errno($curl);
+            $message = curl_error($curl);
+            curl_close($curl);
+            throw new Exception("Network error " . $message . " (" . $error . ")");
+        }
 
-		$statusCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-		curl_close($curl);
-		if($statusCode != 200) {
-			throw new Exception("Status code " . $statusCode . ' ' . $response);
-		}
-		return array( "statusCode" => $statusCode, "body" => $response );
-	}
+        $statusCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        curl_close($curl);
+        if($statusCode != 200) {
+            throw new Exception("Status code " . $statusCode . ' ' . $response);
+        }
+        return array( "statusCode" => $statusCode, "body" => $response );
+    }
 }
